2d7229d8f785503d10c95fdfd59dbaf8db906721
Stuart Raynor <madstu@gmail.com>
2022-03-24 10:31:25 +0000

    Learning CRUD Create Read Update Delete

--- sql-crud.py
+++ sql-crud.py
@@ -10,8 +10,9 @@ db = create_engine("postgresql:///chinook")
 base = declarative_base()
 
 
-# create a class-based model for the "Porgrammer" table
+# create a class-based model for the "Programmer" table
 class Programmer(base):
+    '''create a class-based model for the "Programmer" table'''
     __tablename__ = "Programmer"
     id = Column(Integer, primary_key=True)
     first_name = Column(String)
@@ -24,8 +25,11 @@ class Programmer(base):
 # instead of connection to the databse directly, we will ask fora session
 # create a new instance of sessionmaker, then point to our engine (the db)
 Session = sessionmaker(db)
+
+
 # opens an actual session by calling the Session() subclass defined above
 session = Session()
+
 
 # creating the databse using declarative_base subclass
 base.metadata.create_all(db)
@@ -85,14 +89,58 @@ stu_raynor = Programmer(
     last_name="Raynor",
     gender="M",
     nationality="British",
-    famous_for="01VPS,net"
+    famous_for="01VPS.net"
 )
 
 # add each instance of our programmers to our session
-session.add(stu_raynor)
+# session.add(stu_raynor)
+
+
+# updating a single record
+# programmer = session.query(Programmer).filter_by(id=7).first()
+# programmer.famous_for = "World President"
+
 
 # commit our session to the database
-session.commit()
+# session.commit()
+
+
+# updating multiple records
+# people = session.query(Programmer)
+# for person in people:
+#     if person.gender == "F":
+#         person.gender = "Female"
+#     elif person.gender == "M":
+#         person.gender = "Male"
+#     else:
+#         print("Gender not defined")
+#     session.commit()
+
+
+# deleting a single record
+# fname = input("Enter a first name: ")
+# lname = input("Enter a last name: ")
+# programmer = session.query(Programmer).filter_by(first_name=fname, last_name=lname).first()
+
+# defensive programming
+# if programmer is not None:
+#     print("Programmer Found: ", programmer.first_name + " " + programmer.last_name)
+#     confirmation = input("Are you sure you want to delete this record? (y/n) ")
+#     if confirmation.lower() == "y":
+#         session.delete(programmer)
+#         session.commit()
+#         print("Programmer has been deleted")
+#     else:
+#         print("Programmer not deleted")
+# else:
+#     print("No records found")
+
+# delete multiple records (example)
+# programmers = session.query(Programmer)
+# for programmer in programmers:
+#     session.delete(programmer)
+#     session.commit()
+
 
 # query the database to find all Programmers
 programmers = session.query(Programmer)
